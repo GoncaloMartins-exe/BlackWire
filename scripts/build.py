067eaa140677b72031822c570620fb4d486febbf
GonçaloM <gcm05porto05@gmail.com>
2026-06-16 10:43:36 +0100

    Fix name of executable

--- scripts/build.py
+++ scripts/build.py
@@ -1,21 +1,28 @@
 import os
 import sys
 import subprocess
+from pathlib import Path
 
 def main():
+    # garante que estamos na raiz do projeto (onde está o main.py)
+    project_root = Path(__file__).resolve().parent.parent
+    os.chdir(project_root)
+
     is_windows = sys.platform.startswith("win")
 
-    icon_path = os.path.join("assets", "icons", "LogoBlackWire.png")
+    icon_path = project_root / "assets" / "icons" / "LogoBlackWire.png"
 
     if is_windows:
-        add_data = "assets;assets"
+        add_data = f"assets;assets"
     else:
-        add_data = "assets:assets"
+        add_data = f"assets:assets"
 
     cmd = [
         "pyinstaller",
         "--onefile",
         "--windowed",
+        "--name",
+        "BlackWire",
         f"--add-data={add_data}",
         f"--icon={icon_path}",
         "main.py"
