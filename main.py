c834f974798e779857572c9dcf41d92c701ecd38
GonçaloM <gcm05porto05@gmail.com>
2026-06-16 10:35:57 +0100

    Icon

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 from PySide6.QtGui import QIcon
 from ui.main_window import MainWindow
+from pathlib import Path
+
+def resource_path(path):
+    if hasattr(sys, "_MEIPASS"):
+        return str(Path(sys._MEIPASS) / path)
+    return path
 
 app = QApplication(sys.argv)
 app.setApplicationName("BlackWire")
 app.setOrganizationName("BlackWire")
-app.setWindowIcon(QIcon("assets/icons/LogoBlackWire"))
+app.setWindowIcon(QIcon(resource_path("assets/icons/LogoBlackWire.png")))
 
 window = MainWindow()
 window.show()
